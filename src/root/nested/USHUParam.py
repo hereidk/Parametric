8152104912305f78f84d7e9cf8cdf242131ece0d
hereidk <hereidk@gmail.com>
2014-06-11 16:08:57 -0400

    Comments
    Add comments to USHUParam.py

--- src/root/nested/USHUParam.py
+++ src/root/nested/USHUParam.py
@@ -10,8 +10,13 @@ import numpy as np
 import sys
 
 def HUCatPayout():
+    '''
+    Get user-defined payout structure. Takes any units.
+    AAL calculated in same units, loss cost relative to maximum payout
+    '''
     master = tkinter.Tk()
     
+    # Storm category labels
     tkinter.Label(master, text='Storm Category').grid(row=0)
     tkinter.Label(master, text='TS').grid(row=1)
     tkinter.Label(master, text='Cat 1').grid(row=2)
@@ -20,6 +25,7 @@ def HUCatPayout():
     tkinter.Label(master, text='Cat 4').grid(row=5)
     tkinter.Label(master, text='Cat 5').grid(row=6)
     
+    # Storm category user inputs
     tkinter.Label(master, text='Payout').grid(row=0, column=1)
     e0 = tkinter.Entry(master)
     e1 = tkinter.Entry(master)
@@ -28,6 +34,7 @@ def HUCatPayout():
     e4 = tkinter.Entry(master)
     e5 = tkinter.Entry(master)
     
+    # Default value = 0
     e0.insert(0,0)
     e1.insert(0,0)
     e2.insert(0,0)
@@ -35,6 +42,7 @@ def HUCatPayout():
     e4.insert(0,0)
     e5.insert(0,0)
     
+    # Set window layout
     e0.grid(row=1, column=1)
     e1.grid(row=2, column=1)
     e2.grid(row=3, column=1)
@@ -44,42 +52,25 @@ def HUCatPayout():
     
     e0.focus_set()
     
-    def getPayouts():
+    def getPayouts(): # OK button, gets user payout structure
         payouts = np.zeros((6,1), dtype=float)
-#         payouts[:] = range(6)
         payouts[0] = float(e0.get())
         payouts[1] = float(e1.get())
         payouts[2] = float(e2.get())
         payouts[3] = float(e3.get())
         payouts[4] = float(e4.get())
         payouts[5] = float(e5.get())
-#         print(payouts)
         master.quit()
         global globpayouts
         globpayouts = payouts
         return payouts
     
-    def cancel():
+    def cancel(): # Exit button
         master.destroy()
         sys.exit()
         
-    def createArray(p0,p1,p2,p3,p4,p5):
-        payouts = np.zeros((6,1), dtype=float)
-#         payouts[:] = range(6)
-        payouts[0] = p0.get()
-        payouts[1] = p1.get()
-        payouts[2] = p2.get()
-        payouts[3] = p3.get()
-        payouts[4] = p4.get()
-        payouts[5] = p5.get()
-        print(payouts, payouts[5])
-        return payouts
-    
-    button = tkinter.Button(master, text='OK', command=getPayouts, padx=10)
-#     button = tkinter.Button(master, text='OK', command=lambda p0=e0,p1=e1,p2=e2,p3=e3,p4=e4,p5=e5: createArray(p0,p1,p2,p3,p4,p5))
-    button.grid(row=7, column=0)
+    button = tkinter.Button(master, text='OK', command=getPayouts, padx=10).grid(row=7, column=0)
     tkinter.Button(master, text='Cancel', command = cancel).grid(row=7, column=1)
-    
     
     tkinter.mainloop()
     payouts = button.invoke()
@@ -87,6 +78,9 @@ def HUCatPayout():
     return payouts
 
 def reloadHist(hist_file,reload=True):
+    '''
+    if reload is true, recalculate shapefile, otherwise point to existing shapefile
+    '''
     if reload:
         return param.loadIBTRACSData(hist_file)
     else:
@@ -94,6 +88,10 @@ def reloadHist(hist_file,reload=True):
     
 
 def resultsBox(aal,losscost):
+    '''
+    Text box with results
+    
+    '''
     master = tkinter.Tk()
     
     def cancel():
@@ -108,34 +106,47 @@ def resultsBox(aal,losscost):
 
 
 if __name__ == '__main__':
+    # CSV file with lat/lon points defining box outline
     box_file = r'C:\Python code\Parametric\src\root\nested\Box_template.csv'
+    
+    # Historical dataset, IBTrACS
     hist_file = r'Y:\XP transfer\US\US GoM\Allstorms.ibtracs_wmo.v03r05.csv'
+    
+    
     param = Parametric()
+    
+    # Convert box points to polygon shapefile
     box = param.genParamBox(box_file)
-    ibtracsData = reloadHist(hist_file, reload=False)
+    
+    # Produce shapefile of storm tracks
+    ibtracsData = reloadHist(hist_file, reload=False) # reload=False: Use current shapefile
+    
+    # Get subset of points that fall within box
     intersect = param.intersect(box,ibtracsData)
     
+    # Select highest category that each storm reached within box
     intersect_max = intersect.groupby('Serial', group_keys=False).apply(lambda x: x.ix[x.Category.idxmax()])
     intersect_max.index = range(len(intersect_max))
         
+    # Get user-defined payout structure. Any units or %
     HUCatPayout()
     
+    # Set payout level based on storm category, user inputs
     intersect_max['Payout'] = ''
     for i in intersect_max.index:
         intersect_max.Payout[i] = globpayouts[intersect_max.Category[i],0]
-        
-
-    print(intersect_max)
     
+    # Determine length of historical record
     startYear = 1848
     currentYear = 2013
     yearRange = currentYear - startYear + 1
     
+    # Calculate AAL, loss cost
     totalPayout = sum(intersect_max.Payout.values)
     maxpayout = np.max(globpayouts)
     aal = totalPayout/yearRange
     losscost = aal/maxpayout
     
-    print('AAL=',aal,', Loss Cost=',losscost*100,'%')
+    # Text box with results
     resultsBox(aal,losscost)
     
